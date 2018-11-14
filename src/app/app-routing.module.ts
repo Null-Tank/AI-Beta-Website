784322520294378cdfd235cd8e0304e678aeef29
nabilkhalil <duomaster01@gmail.com>
2018-11-13 17:03:43 -0800

    added components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { DownloadComponent } from './download/download.component';
+import { DevelopersComponent } from './developers/developers.component';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'download', component: DownloadComponent },
+  { path: 'developers', component: DevelopersComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(
+    routes,
+    { enableTracing: true }
+  )],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { DownloadComponent } from './download/download.component';
+import { DevelopersComponent } from './developers/developers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DownloadComponent,
+    DevelopersComponent
   ],
   imports: [
     BrowserModule,
